891be2fc8cbf6fd602d48df2669333e77c0da5cd
Spencer Diniz <spencerdiniz@Spencers-MacBook-Pro-M2-Max.local>
2024-12-17 14:11:01 -0300

    Initial commit. - Blank project. - Added .gitignore

---
+++ busbud-coding-challengeUITests/busbud_coding_challengeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  busbud_coding_challengeUITestsLaunchTests.swift
+//  busbud-coding-challengeUITests
+//
+//  Created by Spencer Diniz on 17/12/24.
+//
+
+import XCTest
+
+final class busbud_coding_challengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
